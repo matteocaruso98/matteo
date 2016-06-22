2d4757559d19c316e2be645875f2737ab4dd79a2
matteocaruso98 <matteocaruso98@hotmail.it>
2016-06-23 00:13:23 +0200

    ciao

--- multiplesoften.py
+++ multiplesoften.py
@@ -3,3 +3,15 @@
 numlist = range (1,11)
 list = [i*10 for i in numlist]
 print("i primi 10 multipli di 10:", list)
+
+#cubes
+cubelist = [i**3 for i in range (1,11)]
+print("i primi dieci cubi:", cubelist)
+
+#awesomeness
+print("i am awesome!!")
+namelist = ["matteo", "francesco", "michele", "chiara", "giorgio"]
+awesomelist = [name + " is aweson" for name in namelist]
+print(awesomelist)
+
+#
